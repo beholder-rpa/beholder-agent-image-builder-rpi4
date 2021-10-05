b6f9520f6d3818e42de886cb92137be49392baf8
Sean McLellan <sean@baristalabs.io>
2021-10-05 00:39:07 -0400

    Add helm and use k3s install directly

--- lib/root/kubeholder_userboot.sh
+++ lib/root/kubeholder_userboot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 git clone --depth=1 https://github.com/beholder-rpa/beholder-iot ~/beholder
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
--- lib/root/kubeholder_userinit.sh
+++ lib/root/kubeholder_userinit.sh
@@ -6,13 +6,12 @@ git clone --depth=1 https://github.com/beholder-rpa/beholder-iot ~/beholder
 arkade get kubectl
 sudo mv /home/beholder/.arkade/bin/kubectl /usr/local/bin/
 
-# Install k3sup
-arkade get k3sup
-sudo mv /home/beholder/.arkade/bin/k3sup /usr/local/bin/
+# Install helm
+arkade get helm
+sudo mv /home/beholder/.arkade/bin/helm /usr/local/bin/
 
 # Install k3s
-echo "# Installing k3s..."
-export IP=$(ifconfig wlan0 | grep -i netmask | awk '{print $2}' | cut -f 2)
-mkdir ~/.kube/
-k3sup install --local --user beholder --local-path ~/.kube/config --context beholder
-echo "# k3s installed."
+curl -sfL https://get.k3s.io | sh -
+
+sudo chown beholder:beholder /etc/rancher/k3s/k3s.yaml
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
